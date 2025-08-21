ff8691824c40a6ba2fe8beebdb5ac61a940b5daa
studycart <rushikeshsunilshahane@gmail.com>
2025-08-21 13:28:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -45,6 +45,10 @@ application.add_handler(CommandHandler("start", start))
 application.add_error_handler(error_handler)
 
 # --- FLASK ROUTES ---
+@app.route('/bot-status')
+def bot_status():
+    return "Bot is running", 200
+
 @app.route('/buy_page')
 def buy_page():
     return render_template('buy_page.html')
