01303cc99b0ccf82abad2271395e52f1e341b374
studycart <rushikeshsunilshahane@gmail.com>
2025-08-23 14:41:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,8 +33,8 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     message_text = (
         "**Unlock the GPay Soundbox Experience! 🔊**\n\n"
         "Get the exclusive GPay Soundbox APK, perfect for demos & pranks, for just **₹39**.\n\n"
-        "🔗 **Join our Channel:** https://t.me/+gmi5eCH5j-wyZDJl\n"
-        "🎥 **Watch Tutorial:** \n https://t.me/+gmi5eCH5j-wyZDJl\n\n"
+        "🔗 **Join our Channel:** https://t.me/gpaysound\n"
+        "🎥 **Watch Tutorial:**  https://t.me/gpaysound\n\n"
         "Click the button below to get your file instantly!"
     )
     # --- END OF UPDATED SECTION ---
